23e14e2b7a9a66d3100d9e1fa62110393b04b5d8
andols-dev <andreasolsson660@gmail.com>
2020-10-26 20:40:35 +0100

    1 binaries uploaded

--- index.tsx
+++ index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Home } from "./components/Home";
 import { About } from "./components/About";
+import {products }from './products'
 import "./style.css";
 
 interface AppProps {}
@@ -23,6 +24,10 @@ class App extends Component<AppProps, AppState> {
   render() {
     return (
       <div>
+
+      {products.map((product, i) => (
+        <div key={i}>{product.id}</div>
+      ))}
         <Router>
           <Switch>
             <Route exact path="/">
