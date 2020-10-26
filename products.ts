4fe51746221eb02f56a45f4d113df3701bb61ecb
andols-dev <andreasolsson660@gmail.com>
2020-10-26 20:18:38 +0100

    add products - products.ts

---
+++ products.ts
@@ -0,0 +1,22 @@
+export interface Products {
+  title: string;
+  price: number;
+  id: string;
+}
+export const products: Products[] = [
+  {
+    title: "TV",
+    price: 23,
+    id: "1"
+  },
+   {
+    title: "Computer",
+    price: 23,
+    id: "2"
+  },
+   {
+    title: "Bicycle",
+    price: 23,
+    id: "3"
+  }
+]
