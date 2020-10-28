5bc71b70839f7fe59d4aa69dd82e92f9b203f5ee
andols-dev <andreasolsson660@gmail.com>
2020-10-28 16:37:36 +0100

    add flex on productinfo

--- components/ProductInfo.tsx
+++ components/ProductInfo.tsx
@@ -12,17 +12,19 @@ export const ProductInfo: React.FC = () => {
   const allProducts = useContext(ProductsContext);
 
   return (
-    <div>
-      {allProducts
-        .filter(product => product.id === id)
-        .map((product, i) => (
-          <div className="flex flex-col" key={i}>
-            <h1>{product.title}</h1>
-            <h1>{product.price}</h1>
-            <Link to="/">Home</Link>
-            <Link to="/hello">Home</Link>
-          </div>
-        ))}
+    <div className="flex justify-center">
+      <div className="w-2/5">
+        {allProducts
+          .filter(product => product.id === id)
+          .map((product, i) => (
+            <div key={i} style={{ border: "1px solid" }}>
+              <h1>{product.title}</h1>
+              <h1>{product.price}</h1>
+              <Link to="/">Home</Link>
+              <Link to="/hello">Home</Link>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
