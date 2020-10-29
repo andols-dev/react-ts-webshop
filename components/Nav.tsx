7c82a916b415e46ebc45a6118a49beebd204ee65
andols-dev <andreasolsson660@gmail.com>
2020-10-29 20:38:14 +0100

    nav modified,change svg clip-rule to clipRule

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -26,7 +26,7 @@ export const Nav: React.FC = () => {
                 <path
                   fillRule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             ) : (
