39e826ee8df0b131906736f922c0a76da1bd1e9e
andols-dev <andreasolsson660@gmail.com>
2020-10-31 18:19:25 +0100

    add top margin to products

--- components/Products.tsx
+++ components/Products.tsx
@@ -6,7 +6,7 @@ export const Products: React.FC = () => {
   const allProducts = useContext(ProductsContext);
   // console.log(allProducts)
   return (
-    <div className="flex justify-center" style={{ border: "1px solid" }}>
+    <div className="flex justify-center mt-10">
       <div className="mt-4 w-full grid md:grid-cols-2 lg:grid-cols-4 gap-4 px-6 py-6">
         {allProducts.map((product, i) => (
           <ProductCard key={i} products={product} />
