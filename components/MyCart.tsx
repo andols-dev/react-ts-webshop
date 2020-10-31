86cb64b7741f9b57f3fcc109d45a727d8ba9066c
andols-dev <andreasolsson660@gmail.com>
2020-10-31 14:55:17 +0100

    add + button and - button

--- components/MyCart.tsx
+++ components/MyCart.tsx
@@ -1,3 +1,42 @@
 import React from "react";
 
-export const MyCart: React.FC = () => <div>Your cart is empty</div>;
+export const MyCart: React.FC = () => {
+  return (
+    <div>
+      <h3>Your cart is empty</h3>
+      <h3>Total cost:</h3>
+      <button>
+        <svg
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </button>
+      <button>
+        <svg
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
