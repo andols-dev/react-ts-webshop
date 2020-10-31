aeec4ff925397b598ed48dbdc0f73dd827903158
andols-dev <andreasolsson660@gmail.com>
2020-10-31 10:40:13 +0100

    add my cart icon and login icon in nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -54,11 +54,43 @@ export const Nav: React.FC = () => {
           <a href="#" className="block hover:bg-gray-800 pt-2 pb-2">
             Home
           </a>
-          <a href="#" className="block hover:bg-gray-800 pt-2 pb-2">
-            Home
+          <a
+            href="#"
+            className="block hover:bg-gray-800 pt-2 pb-2 flex justify-center"
+          >
+            <svg
+              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
           </a>
-          <a href="#" className="block hover:bg-gray-800 pt-2 pb-2">
-            Home
+          <a
+            href="#"
+            className="block hover:bg-gray-800 pt-2 pb-2 flex justify-center"
+          >
+            <svg
+              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+              />
+            </svg>
           </a>
         </div>
       )}
