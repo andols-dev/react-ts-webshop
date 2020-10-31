1903351a94733ebf0f1a4c926f25e992726dcad4
andols-dev <andreasolsson660@gmail.com>
2020-10-31 12:44:02 +0100

    create newsletter component

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Products } from "./Products";
 import { Hero } from "./Hero";
+import { Newsletter } from "./Newsletter";
 export const Home: React.FC = () => {
   return (
     <div>
       <Hero />
       <Products />
+      <Newsletter />
     </div>
   );
 };
---
+++ components/Newsletter.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const Newsletter: React.FC = () => <h1>newsletter</h1>;
