7251c8167381e20115a82f757694452d63537213
andols-dev <andreasolsson660@gmail.com>
2020-10-31 18:52:10 +0100

    add image

--- products.ts
+++ products.ts
@@ -31,6 +31,6 @@ export const products: Products[] = [
     price: 23,
     id: "4",
     img:
-      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+      "https://images.unsplash.com/photo-1484101403633-562f891dc89a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80"
   }
 ];
