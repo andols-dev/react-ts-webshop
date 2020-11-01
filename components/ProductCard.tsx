f40de783478c97b1435b497487f427f1a6f05bf5
andols-dev <andreasolsson660@gmail.com>
2020-11-01 14:36:34 +0100

    remove outline from add to cart btn

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -33,7 +33,23 @@ export const ProductCard: React.FC<Props> = ({
         <button className="bg-gray-200 rounded-md p-2">
           <Link to={`/productinfo/${id}`}>View</Link>
         </button>
-        <button className="bg-gray-200 rounded-md p-2">Add</button>
+        <button className="bg-gray-200 rounded-md p-2 flex focus:outline-none">
+          <span className="mr-1">Add to</span>
+          <svg
+            className="h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
