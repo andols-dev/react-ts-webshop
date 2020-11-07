177df65e57b9cfb68b9239078e9b4de43d99ea93
andols-dev <andreasolsson660@gmail.com>
2020-11-07 12:11:08 +0100

    1 file modified

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,40 +1,39 @@
 import React, { useContext, useState, useEffect } from "react";
 import { ProductsContext } from "../ProductsContext";
 import { ProductCard } from "./ProductCard";
-import "../style.css";
+
 export const Products: React.FC = () => {
   const allProducts = useContext(ProductsContext);
   const [searchValue, setSearchValue] = useState("");
-  const [searchResults, setSearchResults] = useState(allProducts);
 
   const handleChange = e => {
     setSearchValue(e.target.value);
     console.log(e.target.value);
   };
+  const results = !searchValue
+    ? allProducts
+    : allProducts.filter(person =>
+        person.title.toLowerCase().includes(searchValue.toLocaleLowerCase())
+      );
 
-  useEffect(() => {
-    const results = allProducts.filter(product =>
-      product.title.toLowerCase().includes(searchValue)
-    );
-    setSearchResults(results);
-  }, [searchValue]);
   // console.log(allProducts)
-  
+  console.log(allProducts);
   return (
     <div>
-      <div className="px-6 mt-10">
+      <div className="text-center mt-10">
         <input
-          className="px-2 py-2 border"
           type="text"
+          className="border"
           placeholder="search products..."
           value={searchValue}
           onChange={handleChange}
         />
+        <input type="submit" value="Submit" />
       </div>
 
       <div className="flex justify-center ">
         <div className="w-full grid md:grid-cols-2 lg:grid-cols-4 gap-4 px-6 py-6">
-          {searchResults.map((product, i) => (
+          {results.map((product, i) => (
             <ProductCard key={i} products={product} />
           ))}
         </div>
