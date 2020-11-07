220cd8a9aeaf3077f9c83ef0a8950a8636e69988
andols-dev <andreasolsson660@gmail.com>
2020-11-07 17:17:05 +0100

    1 file modified, creating login form

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,3 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export const Login: React.FC = () => <div>Login/become a member</div>;
+export const Login: React.FC = () => {
+  const formDefaultValues = {
+    userName: "",
+    password: ""
+  };
+  const [formValues, setFormValues] = useState(formDefaultValues);
+  const { userName, password } = formValues;
+
+  useEffect(() => {
+    console.log({ userName, password });
+  }, [formValues]);
+
+  const handleChange = e => {
+    const target = e.target;
+    setFormValues(prevState => ({
+      ...prevState,
+      [target.name]: target.value
+    }));
+  };
+  return (
+    <div>
+      <form className="flex flex-col">
+        <label>Username:</label>
+        <input
+          type="text"
+          name={"userName"}
+          className="border"
+          value={userName}
+          onChange={handleChange}
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          name={"password"}
+          className="border"
+          value={password}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
+};
